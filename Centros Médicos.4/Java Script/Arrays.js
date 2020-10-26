a022e86d29eb9463989b49bc0d3c79eaef4228ed
Cinthya <cnm.ramos@yavirac.edu.ec>
2020-10-26 16:47:50 -0500

    Cuarta Actualización

---
+++ Centros Médicos.4/Java Script/Arrays.js
@@ -0,0 +1,354 @@
+var Barrios=[
+"La_Tola", "La_Vicentina", "Jardines_del_Valle","Centro_Histórico","El_Placer","La_Basílica","Plaza_Arenas","Gangotena_Posse","Toctiuco"
+,"Chimbacalle","Chiriacu_Bajo","Chiriacu_Alto","Ferroviara_Baja","Ferroviaria_Alta","La_Forestal","Luluncoto","Obrero_Independiente"
+,"Barrio_Paluco","Pio_XII","Puengsí_1","Puengsí_2","Puengsí_3","Unión_de_Ciudadelas","Ciudadela_Atahualpa","Gatazo","Jesús_del_Gran_Poder"
+,"La_Raya","Lloa","Promoción_Familiar","Quito_Sur","Tarque","La_Libertad","Nueva_Aurora","La_Ermita","Lucha_de_los_Pobres","Aída_León"
+,"Hierba_Buena_1","Hierba_Buena_2","La_Argelia","El_Carmen_Mercado_Mayorista_Calle_Proceso","Oriente_Quiteño","Sangolquí","Capelo"
+,"Cotogchoa","Curipungo","Fajardo","Jutumpungo","San_Fer_De_Sangolquí","Selva_Alegre","Rumipamba","Alóag","Aloasí","Chaupi","Tambillo"
+,"Uyumbicho","Cutuglahua","Tandapi","Guamaní","Asistencia_Social","Caupicho","El_Blanqueado","El_Rocío","Matilde_Álvarez","Pueblo_Unido"
+,"San_Martín_de_Porres","Victoria_Central","Ciudadela_Ibarra","Chillogallo","El_Tránsito","Isla_Solanda","La_Ecuatoriana","La_Inmaculada"
+,"Martha_Bucaram","San_Luis","Santa_Bárbara","Santa_Cruz","Turubamba","Buena_Ventura","Conocoto","Alangasí","Amaguaña","Cuendina"
+,"El_Tingo","Guangopolo","La_Merced","Píntag","Tolontag"];
+var La_Tola= [
+   "Ríos y Clhile", "3161801  Ext 220","Medicina General, Ginecología, Obstetricia, Psicología, Laboratorio clínico, Nutrición, Trabajo Social, Profilaxis"
+];
+
+var La_Vicentina= [
+   "Av. Oriental 248 y Cajías Vicentina Alta	", "2526714","Medicina General, Obstetricia, Odontología"
+];
+
+var Jardines_del_Valle= [
+   "Eloy  Alfaro", "2601491","Medicina General, Obstetricia, Odontología",""
+];
+
+var Centro_Histórico= [
+   "Rocafuerte 1545 y Venezuela","2283017  Ext 2283","Medicina General Pediatría Ginecología Obstetricia Emergencia Maternidad de corta estancia Ecografía Laboratorio Rayos X Terapia familiar Odontología Salud Mental Profilaxis"
+];
+
+var El_Placer= [
+   "El Placer 1317","3151370","Medicina General Peditría Obstetricia Odontología"
+];
+
+var La_Basílica= [
+   "Carchi y Venezuela","3151711","Medicina General Peditría Obstetricia Odontología"
+];
+
+var Plaza_Arenas= [
+   "Vargas 444 y Galápagos","2952980","Medicina General Pediatría Obstetricia Odontología"
+];
+
+var Gangotena_Posse= [
+   "Rocafuerte 834-","2281135","Cuidado Infantil Medicina General Pediatría Ginecología Obstetricia Laboratorio Odontología Salud Mental"
+];
+
+var Toctiuco= [
+   "PS, Allende 755 y Alondo de Cevallos","2952600","Medicina General Pediatría Obstetricia Odontología"
+];
+
+var Chimbacalle= [
+   "Av. Napo y J. del Alcázar Chimbacalle","2657407","Emergencia, Maternidad de corta estancia, Ecografía, Laboratorio, Rayos X, Terapia  familiar, Odontología, Educación Popular"
+];
+
+var Chiriacu_Bajo= [
+   "Martín de la Calle y Hernán Morillo","2658068","Medicina General, Pediatría, Ginecología"
+];
+
+var Chiriacu_Alto= [
+   "Gonzalo Marín s/n y Sumbá","2610488","Medicina General, Pediatría, Ginecología"
+];
+
+var Ferroviara_Baja= [
+   "Juan Cueva García","2655494","Medicina General, Pediatría, Ginecología, Psicología"
+];
+
+var Ferroviara_Alta= [
+   "Eduardo Vorley s/n","2658029","Medicina General, Pediatría, Ginecología, Psicología"
+];
+
+var La_Forestal= [
+   "Chicán y Omaguas","2648673","Medicina General, Pediatría, Ginecología"
+];
+
+var Luluncoto= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Obrero_Independiente= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Barrio_Paluco= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Pio_XII= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Puengsí_1= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Puengsí_2= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+    
+var Puengsí_3= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+
+var Unión_de_Ciudadelas= [
+   "","","Medicina General, Pediatría, Ginecología"
+];
+  
+var Ciudadela_Atahualpa= [
+   "","",""
+];
+
+var Gatazo= [
+   "","",""
+   ];
+      
+var Jesús_del_Gran_Poder= [
+   "","",""
+];
+
+var La_Raya= [
+   "","",""
+   ];
+   
+var Lloa= [
+   "","",""
+   ];
+
+var Promoción_Familiar= [
+   "","",""
+];
+
+var Quito_Sur= [
+   "","",""
+];
+
+var Tarque= [
+   "","",""
+];
+
+var La_Libertad= [
+   "","",""
+];
+
+var Nueva_Aurora= [
+   "","",""
+];
+
+var La_Ermita= [
+   "","",""
+];
+
+var Lucha_de_los_Pobres= [
+   "","",""
+];
+         
+var Aída_León= [
+   "","",""
+];
+         
+var Hierba_Buena_1= [
+   "","",""
+];
+      
+var Hierba_Buena_2= [
+   "","",""
+];
+         
+var La_Argelia= [
+   "","",""
+];
+         
+var El_Carmen_Mercado_Mayorista_Calle_Proceso= [
+   "","",""
+];
+         
+var Oriente_Quiteño= [
+   "","",""
+];
+
+var Sangolquí= [
+   "","",""
+];
+            
+var Capelo= [
+   "","",""
+];
+            
+var Cotogchoa= [
+   "","",""
+];
+            
+var Curipungo= [
+   "","",""
+];
+            
+var Fajardo= [
+   "","",""
+];
+      
+var Jutumpungo= [
+   "","",""
+];
+            
+var San_Fer_De_Sangolquí= [
+   "","",""
+];
+            
+var Selva_Alegre= [
+   "","",""
+];
+            
+var Rumipamba= [
+   "","",""
+];
+            
+var Alóag= [
+   "","",""
+];
+               
+var Aloasí= [
+   "","",""
+];
+
+var Chaupi= [
+   "","",""
+];
+               
+var Tambillo= [
+   "","",""
+];
+               
+var Uyumbicho= [
+   "","",""
+];
+               
+var Cutuglahua= [
+   "","",""
+];
+        
+var Tandapi= [
+   "","",""
+];
+   
+var Guamaní= [
+   "","",""
+];
+               
+var Asistencia_Social= [
+   "","",""
+];
+               
+var Caupicho= [
+   "","",""
+];
+               
+var El_Blanqueado= [
+   "","",""
+];
+                  
+var El_Rocío= [
+   "","",""
+];
+                  
+var Matilde_Álvarez= [
+   "","",""
+];
+   
+var Pueblo_Unido= [
+   "","",""
+];
+                  
+var San_Martín_de_Porres= [
+   "","",""
+];
+      
+var Victoria_Central= [
+   "","",""
+];
+                  
+var Ciudadela_Ibarra= [
+   "","",""
+];
+   
+var Chillogallo= [
+   "","",""
+];
+                  
+var El_Tránsito= [
+   "","",""
+];
+                  
+var Isla_Solanda= [
+   "","",""
+];
+                  
+var La_Ecuatoriana= [
+   "","",""
+];
+
+var La_Inmaculada= [
+   "","",""
+];
+                     
+var Martha_Bucaram= [
+   "","",""
+];
+
+var San_Luis= [
+   "","",""
+];
+                     
+var Santa_Bárbara= [
+   "","",""
+];
+                     
+var Santa_Cruz= [
+   "","",""
+];
+                     
+var Turubamba= [
+   "","",""
+];
+
+var Buena_Ventura= [
+   "","",""
+];
+
+var Conocoto= [
+   "","",""
+];
+
+var Alangasí= [
+   "","",""
+];
+
+var Amaguaña= [
+   "","",""
+];
+
+var Cuendina= [
+   "","",""
+];
+
+var El_Tingo= [
+   "","",""
+];
+var Guangopolo= [
+   "","",""
+];
+var La_Merced= [
+   "","",""
+];
+var Píntag= [
+   "","",""
+];
+var Tolontag= [
+   "","",""
+];
---
+++ Centros Médicos.4/Java Script/Variables.js
@@ -0,0 +1,4 @@
+var Logo_Simple=document.getElementById("Logo-Simple");
+var BuscadorC=document.getElementById("BuscadorC");
+var Contenedor=document.getElementById("Contenedor");
+var Bordes=document.getElementById("Bordes");
