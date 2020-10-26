1485a9a19d49f0b4717dc9fa519c40a3bc3d9cf9
Jhon.T <jav.tanicuchi@yavirac.edu.ec>
2020-10-26 02:08:43 -0500

    Ventana Popup, Sistema de sintomas

--- Our Salute_Test/js/variables.js
+++ Our Salute_Test/js/variables.js
@@ -1,3 +1,22 @@
+/* --------- */
+/* Pantallas */
+/* --------- */
+
 var pantallaUno= document.getElementById("pantalla-uno");
 var pantallaDos= document.getElementById("pantalla-dos");
+var pantallaTres= document.getElementById("pantalla-tres");
+var pantallaCuatro= document.getElementById("pantalla-cuatro");
 
+
+/* ----------------------- */
+/* VALORES DE ENFERMEDADES */
+/* ----------------------- */
+
+var valorDiabetes;
+var valorColelitiasis;
+var valorNeumonia;
+var valorGastroentesteritis;
+var valorApendicitis;
+
+
+var ul = document.getElementById('lista-sintomas');
