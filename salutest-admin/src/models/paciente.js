4f2c9e3cf558f4aa8c67f53e8b4fdab5d7eb7fd9
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-03-31 22:56:44 -0500

    creacion modelos: persona,empleado,paciente,usuario_empleado,usuario_paciente. Y modificacion de login y estilos

--- salutest-admin/src/models/paciente.js
+++ salutest-admin/src/models/paciente.js
@@ -1,19 +1,20 @@
-const catalogo = (sequelize, type) => {
+const paciente = (sequelize, type) => {
   return sequelize.define(
-    "cliente",
+    "paciente",
     {
-      idCatalogo: {
+      id_paciente: {
         type: type.INTEGER,
         primaryKey: true,
         autoIncrement: true,
       },
-      nombre: type.STRING(99),
-      creacionCliente: {
+      donador: type.BOOLEAN,
+      estado: type.STRING,
+      creacionUsuarios: {
         type: "TIMESTAMP",
         defaultValue: type.literal("CURRENT_TIMESTAMP"),
         allowNull: false,
       },
-      actualizacionCliente: {
+      actualizacionUsuarios: {
         type: "TIMESTAMP",
         defaultValue: type.literal("CURRENT_TIMESTAMP "),
         allowNull: false,
@@ -25,4 +26,4 @@ const catalogo = (sequelize, type) => {
   );
 };
 
-module.exports = catalogo;
+module.exports = paciente;
