eed64771bb5826778d244f1ec3aac00b4059f4a3
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-04 23:31:44 -0500

    creacion modelo: institucion_medica

---
+++ salutest-admin/src/models/institucion_medica.js
@@ -0,0 +1,31 @@
+const institucion_medica = (sequelize, type) => {
+  return sequelize.define(
+    "institucion_medica",
+    {
+      id_institucion_medica: {
+        type: type.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      nombre: type.STRING,
+      descripcion: type.STRING,
+      fecha: type.STRING,
+      estado: type.BOOLEAN,
+      creacioninstituciones_medicas: {
+        type: "TIMESTAMP",
+        defaultValue: type.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+      },
+      actualizacioninstituciones_medicas: {
+        type: "TIMESTAMP",
+        defaultValue: type.literal("CURRENT_TIMESTAMP "),
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
+
+module.exports = institucion_medica;
