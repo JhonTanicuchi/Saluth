1f965fd2ef1c1d0e90c0bbcc77d730f5e60cca78
Jenny <86363759+jennifher23@users.noreply.github.com>
2022-04-06 09:51:07 -0500

    Creacion del modelo area

--- salutest-admin/src/config-database/database.orm.js
+++ salutest-admin/src/config-database/database.orm.js
@@ -27,7 +27,7 @@ const area_empleado_Modelo = require("../models/area_empleado");
 const atencionMedica_citaMedica_Modelo = require("../models/atencionMedica_citaMedica");
 const diagnostico_Modelo = require("../models/diagnostico");
 const diagnostico_enfermedad_Modelo = require("../models/diagnostico_enfermedad");
-
+const area_Modelo = require("../models/area");
 
 
 const sequelize = new Sequelize("database_salutest", "root", "", {
@@ -61,13 +61,11 @@ const usuario_empleado = usuarioEmpleado_Modelo(sequelize, Sequelize);
 const usuario_paciente = usuarioPaciente_Modelo(sequelize, Sequelize);
 const enfermedad = enfermedad_Modelo(sequelize, Sequelize);
 const cita_medica = cita_medica_Modelo(sequelize, Sequelize);
-<<<<<<< HEAD
 const area_empleado = area_empleado_Modelo(sequelize, Sequelize);
-=======
 const atencionMedica_citaMedica = atencionMedica_citaMedica_Modelo(sequelize, Sequelize);
 const id_diagnostico = diagnostico_Modelo(sequelize, Sequelize);
 const id_diagnostico_enfermedad = diagnostico_enfermedad_Modelo(sequelize, Sequelize);
->>>>>>> 96974cbdf82f26384c9b5e7d4233c01e0ad7ef3f
+const area = area_Modelo(sequelize, Sequelize);
 
 
 
