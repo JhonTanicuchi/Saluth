f09ff90ba4978a914179a9baf60abb0ecfd0bcd4
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-07 18:08:40 -0500

    solution restore database.orm

--- salutest-admin/src/config-database/database.orm.js
+++ salutest-admin/src/config-database/database.orm.js
@@ -40,7 +40,15 @@ const sub_area_Modelo = require("../models/sub_area");
 const equipo_medico_Modelo = require("../models/equipo_medico");
 const transmision_Modelo = require("../models/transmision");
 const enfermedad_sintoma_Modelo = require("../models/enfermedad_sintoma");
-
+const rol_Modelo = require("../models/rol");
+const rol_permiso_Modelo = require("../models/rol_permiso");
+const permiso_Modelo = require("../models/permiso");
+const area_especialidade_Modelo = require("../models/area_especialidad");
+const especialidad_Modelo = require("../models/especialidad");
+const cargo_Modelo = require("../models/cargo");
+const atencion_medica_Modelo = require("../models/atencion_medica");
+const tipo_examenes_Modelo = require("../models/tipo_examen");
+const receta_medica_Modelo = require("../models/receta_medica");
 
 const sequelize = new Sequelize("database_salutest", "root", "", {
   host: "localhost",
@@ -74,9 +82,15 @@ const usuario_paciente = usuarioPaciente_Modelo(sequelize, Sequelize);
 const enfermedad = enfermedad_Modelo(sequelize, Sequelize);
 const cita_medica = cita_medica_Modelo(sequelize, Sequelize);
 const area_empleado = area_empleado_Modelo(sequelize, Sequelize);
-const atencionMedica_citaMedica = atencionMedica_citaMedica_Modelo(sequelize, Sequelize);
+const atencionMedica_citaMedica = atencionMedica_citaMedica_Modelo(
+  sequelize,
+  Sequelize
+);
 const diagnostico = diagnostico_Modelo(sequelize, Sequelize);
-const diagnostico_enfermedad = diagnostico_enfermedad_Modelo(sequelize, Sequelize);
+const diagnostico_enfermedad = diagnostico_enfermedad_Modelo(
+  sequelize,
+  Sequelize
+);
 const area = area_Modelo(sequelize, Sequelize);
 const tipo_examene = tipo_examene_Modelo(sequelize, Sequelize);
 const examen = examen_Modelo(sequelize, Sequelize);
@@ -90,6 +104,15 @@ const sub_area = sub_area_Modelo(sequelize, Sequelize);
 const equipo_medico = equipo_medico_Modelo(sequelize, Sequelize);
 const transmision = transmision_Modelo(sequelize, Sequelize);
 const enfermedad_sintoma = enfermedad_sintoma_Modelo(sequelize, Sequelize);
+const rol = rol_Modelo(sequelize, Sequelize);
+const rol_permiso = rol_permiso_Modelo(sequelize, Sequelize);
+const permiso = permiso_Modelo(sequelize, Sequelize);
+const receta_medica = receta_medica_Modelo(sequelize, Sequelize);
+const area_especialidade = area_especialidade_Modelo(sequelize, Sequelize);
+const especialidad = especialidad_Modelo(sequelize, Sequelize);
+const cargo = cargo_Modelo(sequelize, Sequelize);
+const atencion_medica = atencion_medica_Modelo(sequelize, Sequelize);
+
 //relaciones
 
 persona.hasMany(empleado);
@@ -110,4 +133,31 @@ module.exports = {
   paciente,
   usuario_empleado,
   usuario_paciente,
+  enfermedad,
+  cita_medica,
+  area_empleado,
+  atencionMedica_citaMedica,
+  diagnostico,
+  diagnostico_enfermedad,
+  area,
+  tipo_examene,
+  examen,
+  mensaje,
+  medicamento,
+  turno,
+  horario,
+  sala,
+  sintoma,
+  sub_area,
+  equipo_medico,
+  transmision,
+  enfermedad_sintoma,
+  rol,
+  rol_permiso,
+  permiso,
+  receta_medica,
+  area_especialidade,
+  especialidad,
+  cargo,
+  atencion_medica,
 };
