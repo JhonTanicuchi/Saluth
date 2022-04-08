a25515609f5f2815a134bf805f0c2c54a0f70578
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-08 01:08:43 -0500

    definition of the routes

--- salutest-web/src/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-const index = {}
-
-index.mostrar = (req,res) => {
-    res.render ('modules/salutest-patient')
-}
-
-module.exports = index
--- salutest-web/src/controllers/job.controller.js
+++
@@ -1,7 +0,0 @@
-const job = {}
-
-job.mostrar = (req,res) => {
-    res.render ('job')
-}
-
-module.exports = job
