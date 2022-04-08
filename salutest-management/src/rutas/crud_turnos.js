96f304e5746949c13dcd63130f632306453a2241
Thalia Guzñay <jtc.guznay@yavirac.edu.ec>
2022-04-08 10:58:07 -0500

    corregir errores

--- salutest-management/src/rutas/crud_turnos.js
+++ salutest-management/src/rutas/crud_turnos.js
@@ -2,12 +2,8 @@ const express = require('express');
 const rutas = express.Router();
 
 const {mostrar} = require('../controllers/crud_turnos.controller');
-<<<<<<< HEAD:salutest-admin/src/rutas/crud_turnos.js
 const {check_login} = require('../lib/auth');
-=======
-const {check_login} = require("../lib/auth");
->>>>>>> ed6726e13d8878eed893a78bdf6f94117806ece3:salutest-management/src/rutas/crud_turnos.js
 
-rutas.get('/crud_turnos', mostrar);
+rutas.get('/crud_turnos',check_login, mostrar);
 
 module.exports = rutas;
