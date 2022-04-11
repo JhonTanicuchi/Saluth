c69101f2736b08471ae80169f7bb023863d87197
joeiv023 <jic.guevara@yavirac.edu.ec>
2022-04-11 16:02:16 -0500

    creacion modelo: recetaMedica_medicamentos

--- salutest-management/src/config-database/database.orm.js
+++ salutest-management/src/config-database/database.orm.js
@@ -4,17 +4,17 @@ const mysql = require("mysql2/promise");
 const dbName = process.env.DB_SCHEMAS || "database_salutest";
 
 mysql
-  .createConnection({
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || "3306",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-  })
-  .then((connection) => {
-    connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`).then((res) => {
-      console.info("Base de datos creada o comprobada correctamente");
+    .createConnection({
+        host: process.env.DB_HOST || "127.0.0.1",
+        port: process.env.DB_PORT || "3306",
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "",
+    })
+    .then((connection) => {
+        connection.query(`CREATE DATABASE IF NOT EXISTS ${dbName};`).then((res) => {
+            console.info("Base de datos creada o comprobada correctamente");
+        });
     });
-  });
 
 const persona_Modelo = require("../models/persona");
 const empleado_Modelo = require("../models/empleado");
@@ -42,6 +42,7 @@ const enfermedad_sintoma_Modelo = require("../models/enfermedad_sintoma");
 const rol_Modelo = require("../models/rol");
 const rol_permiso_Modelo = require("../models/rol_permiso");
 const permiso_Modelo = require("../models/permiso");
+const recetaMedica_medicamento = require("../models/recetaMedica_medicamento");
 const area_especialidade_Modelo = require("../models/area_especialidad");
 const especialidad_Modelo = require("../models/especialidad");
 const cargo_Modelo = require("../models/cargo");
@@ -56,27 +57,27 @@ const area_citaMedica_Modelo = require("../models/area_citaMedica");
 const historia_clinica_Modelo = require("../models/historia_clinica");
 
 const sequelize = new Sequelize("database_salutest", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    require: 30000,
-    idle: 10000,
-  },
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        require: 30000,
+        idle: 10000,
+    },
 });
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Conectado");
-  })
-  .catch((err) => {
-    console.log("No se conecto");
-  });
+    .authenticate()
+    .then(() => {
+        console.log("Conectado");
+    })
+    .catch((err) => {
+        console.log("No se conecto");
+    });
 
 sequelize.sync({ force: false }).then(() => {
-  console.log("Tablas sincronizadas");
+    console.log("Tablas sincronizadas");
 });
 
 const persona = persona_Modelo(sequelize, Sequelize);
@@ -88,13 +89,13 @@ const enfermedad = enfermedad_Modelo(sequelize, Sequelize);
 const cita_medica = cita_medica_Modelo(sequelize, Sequelize);
 const area_empleado = area_empleado_Modelo(sequelize, Sequelize);
 const atencionMedica_citaMedica = atencionMedica_citaMedica_Modelo(
-  sequelize,
-  Sequelize
+    sequelize,
+    Sequelize
 );
 const diagnostico = diagnostico_Modelo(sequelize, Sequelize);
 const diagnostico_enfermedad = diagnostico_enfermedad_Modelo(
-  sequelize,
-  Sequelize
+    sequelize,
+    Sequelize
 );
 const area = area_Modelo(sequelize, Sequelize);
 const tipo_examen = tipo_examenes_Modelo(sequelize, Sequelize);
@@ -112,6 +113,7 @@ const enfermedad_sintoma = enfermedad_sintoma_Modelo(sequelize, Sequelize);
 const rol = rol_Modelo(sequelize, Sequelize);
 const rol_permiso = rol_permiso_Modelo(sequelize, Sequelize);
 const permiso = permiso_Modelo(sequelize, Sequelize);
+const recetaMedica_medicamento = recetaMedica_medicamento_Modelo(sequelize, Sequelize);
 const receta_medica = receta_medica_Modelo(sequelize, Sequelize);
 const area_especialidad = area_especialidade_Modelo(sequelize, Sequelize);
 const especialidad = especialidad_Modelo(sequelize, Sequelize);
@@ -139,36 +141,36 @@ paciente.hasMany(usuario_paciente);
 usuario_paciente.belongsTo(paciente);
 
 module.exports = {
-  persona,
-  empleado,
-  paciente,
-  usuario_empleado,
-  usuario_paciente,
-  enfermedad,
-  cita_medica,
-  area_empleado,
-  atencionMedica_citaMedica,
-  diagnostico,
-  diagnostico_enfermedad,
-  area,
-  tipo_examen,
-  examen,
-  mensaje,
-  medicamento,
-  turno,
-  horario,
-  sala,
-  sintoma,
-  sub_area,
-  equipo_medico,
-  transmision,
-  enfermedad_sintoma,
-  rol,
-  rol_permiso,
-  permiso,
-  receta_medica,
-  area_especialidad,
-  especialidad,
-  cargo,
-  atencion_medica,
+    persona,
+    empleado,
+    paciente,
+    usuario_empleado,
+    usuario_paciente,
+    enfermedad,
+    cita_medica,
+    area_empleado,
+    atencionMedica_citaMedica,
+    diagnostico,
+    diagnostico_enfermedad,
+    area,
+    tipo_examen,
+    examen,
+    mensaje,
+    medicamento,
+    turno,
+    horario,
+    sala,
+    sintoma,
+    sub_area,
+    equipo_medico,
+    transmision,
+    enfermedad_sintoma,
+    rol,
+    rol_permiso,
+    permiso,
+    receta_medica,
+    area_especialidad,
+    especialidad,
+    cargo,
+    atencion_medica,
 };
