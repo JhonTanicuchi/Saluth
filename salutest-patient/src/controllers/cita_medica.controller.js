5fcdefcefc1ae9ca353b89179d401c378b3cfa00
Brayan Quilo <bsg.quilo@yavirac.edu.ec>
2022-04-11 22:34:45 -0500

    Modify file -> cita_medica

---
+++ salutest-patient/src/controllers/cita_medica.controller.js
@@ -0,0 +1,7 @@
+const cita_medica = {}
+
+cita_medica.mostrar = (req,res) => {
+    res.render ('modules/cita_medica')
+}
+
+module.exports = cita_medica
---
+++ salutest-patient/src/public/js/cita_medica.js
@@ -0,0 +1,26 @@
+function record_filter() {
+    // Declare variables
+    var input, filter, table, tr, td, i, j, visible;
+    var input = document.getElementById("record_filter");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("record_table");
+    tr = table.getElementsByTagName("tr");
+  console.log(filter)
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      visible = false;
+      /* Obtenemos todas las celdas de la fila, no sólo la primera */
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+        if (td[j] && td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+          visible = true;
+        }
+      }
+      if (visible === true) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+  
