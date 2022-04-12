42a96e69f516f088a6bdcb628be41006186f3599
unknown <dbb.haro@yavirac.edu.ec>
2022-04-11 23:42:37 -0500

    arreglo de los nombres

--- salutest-management/src/app.js
+++ salutest-management/src/app.js
@@ -90,23 +90,17 @@ app.use(require('./rutas/empleados'))
 app.use(require('./rutas/roles'))
 app.use(require('./rutas/medicamentos'))
 app.use(require('./rutas/especialidades'))
-app.use(require('./rutas/pacientes'))
 app.use(require('./rutas/vistacrudcitamedica'))
 app.use(require('./rutas/listacitamedica'))
-app.use(require('./rutas/vistacrudhorarios'))
 app.use(require('./rutas/usuarios_pacientes'))
-app.use(require('./rutas/vistaempleadosdb'))
-app.use(require('./rutas/horarios'))
-app.use(require('./rutas/usuarios_pacientes'))
-app.use(require('./rutas/empleado'))
 app.use(require('./rutas/receta_medica'))
 app.use(require('./rutas/signos_vitales'))
 app.use(require('./rutas/perfil'))
+app.use(require('./rutas/pacientes'))
+app.use(require('./rutas/horarios'))
+app.use(require('./rutas/empleados'))
+
 /* app.use('/modules', require('./routes/inquiries')) */
 /* app.use('/modules', require('./routes/inquiries')) */
-<<<<<<< HEAD
-=======
-    /* app.use('/modules', require('./routes/inquiries')) */
->>>>>>> 8f0a1995956effaf80c1c4791c90f30da2936aae
 
 module.exports = app;
