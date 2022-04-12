bc5d34d7897fc64038f1d21fe8ff9011d9707de1
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-11 22:08:11 -0500

    config nav, and depuration

--- salutest-erp/src/app.js
+++ salutest-erp/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 require('./lib/passport');
 
 /// archivos compartidos
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
