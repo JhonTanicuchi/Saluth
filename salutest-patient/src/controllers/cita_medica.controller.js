a3fe926d6bc1301c5938c6bfe968ea0f88049483
Brayan Quilo <bsg.quilo@yavirac.edu.ec>
2022-04-14 16:50:38 -0500

    Edit medical appointment table ->controller.js and cita_medica.hbs

--- salutest-patient/src/controllers/cita_medica.controller.js
+++ salutest-patient/src/controllers/cita_medica.controller.js
@@ -6,7 +6,7 @@ cita_medica.mostrar = (req,res) => {
 cita_medica.list = async (req, res) => {
     const citas_medicas = await sql.query("SELECT * FROM cita_medica");
     console.log(cita_medica)
-    res.render('modules/cita_medica', {citas_medicas});
+    res.render('modules/cita_medica', {cita_medica});
 };
 
 module.exports = cita_medica
