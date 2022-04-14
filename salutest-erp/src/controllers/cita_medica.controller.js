d5045125d6df0c4fe180910839e06db471db2545
Jenny <86363759+jennifher23@users.noreply.github.com>
2022-04-13 21:38:42 -0500

    Modificaciones

--- salutest-erp/src/controllers/cita_medica.controller.js
+++ salutest-erp/src/controllers/cita_medica.controller.js
@@ -1,7 +1,31 @@
+const orm = require("../config-database/database.orm");
+const sql = require("../config-database/database.sql");
+
 const cita_medica = {}
 
 cita_medica.mostrar = (req,res) => {
     res.render ('modules/cita_medica')
-}
+};
 
-module.exports = cita_medica
+cita_medica.save =async (req, res) => {
+    const{ ID, Cedula, Nombre, Correo, Ciudad } =
+        req.body;
+    const nuevaCita ={
+        ID,
+        Cedula,
+        Nombre,
+        Correo,
+        Ciudad,
+    };
+
+    await orm.cita_medica.create(nuevaCita);
+    res.redirect("/");
+};
+
+module.exports = cita_medica;
+
+cita_medica.list = async (req, res) => {
+    const citas_medicas = await sql.query("SELECT * FROM cita medicas");
+    console.log(cita_medica)
+    res.render('modules/cita_medica', {citas_medicas});
+};
--- salutest-erp/src/rutas/cita_medica.js
+++ salutest-erp/src/rutas/cita_medica.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const rutas = express.Router();
 
-const {mostrar} = require('../controllers/cita_medica.controller');
+const cita_medicaController = require('../controllers/cita_medica.controller');
 const {check_login} = require('../lib/auth');
 
-rutas.get('/cita_medica',check_login,mostrar);
+rutas.get('/cita_medica',check_login, cita_medicaController.list);
 
 module.exports = rutas;
