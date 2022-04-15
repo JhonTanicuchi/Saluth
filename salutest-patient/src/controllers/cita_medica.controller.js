c96287209330b56b694bbe9c1a9f59800c93156d
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-15 00:17:06 -0500

    depuration

--- salutest-patient/src/controllers/cita_medica.controller.js
+++ salutest-patient/src/controllers/cita_medica.controller.js
@@ -4,11 +4,11 @@ const sql = require("../config-database/database.sql");
 const cita_medica = {};
 
 cita_medica.mostrar = (req, res) => {
-  res.render("modules/cita_medica");
+  res.render("modules/record_citas");
 };
 cita_medica.list = async (req, res) => {
   const citas_medicas = await sql.query("SELECT * FROM cita_medicas");
-  res.render("modules/cita_medica", { citas_medicas });
+  res.render("modules/record_citas", { citas_medicas });
 };
 
 module.exports = cita_medica;
