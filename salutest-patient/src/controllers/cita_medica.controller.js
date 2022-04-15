3afc3347b07b7ccd1f8647ee4936732eabdc44a5
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-14 19:43:16 -0500

    config: read-cita_medica

--- salutest-patient/src/controllers/cita_medica.controller.js
+++ salutest-patient/src/controllers/cita_medica.controller.js
@@ -1,13 +1,14 @@
-const cita_medica = {}
+const orm = require("../config-database/database.orm");
+const sql = require("../config-database/database.sql");
 
-cita_medica.mostrar = (req,res) => {
-    res.render ('modules/cita_medica')
-}
+const cita_medica = {};
+
+cita_medica.mostrar = (req, res) => {
+  res.render("modules/cita_medica");
+};
 cita_medica.list = async (req, res) => {
-    const citas_medicas = await sql.query("SELECT * FROM cita_medica");
-    console.log(cita_medica)
-    res.render('modules/cita_medica', {cita_medica});
+  const citas_medicas = await sql.query("SELECT * FROM cita_medicas");
+  res.render("modules/cita_medica", { citas_medicas });
 };
 
-module.exports = cita_medica
-
+module.exports = cita_medica;
