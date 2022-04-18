60421f91308c614949a923e26579b836c50c005c
Thalia Guzñay <jtc.guznay@yavirac.edu.ec>
2022-04-17 20:48:12 -0500

    crud_especialidades

--- salutest-business/src/controllers/especialidades.controller.js
+++ salutest-business/src/controllers/especialidades.controller.js
@@ -1,7 +1,30 @@
+const orm = require("../config-database/database.orm");
+const sql = require("../config-database/database.sql");
+
 const especialidades = {}
 
 especialidades.mostrar = (req,res) => {
-    res.render ('modules/especialidades')
+    res.render ('modules/especialidades');
+};
+
+especialidades.save = async (req, res) => {
+    const {especialidad, descripcion, fecha_creacion, estado} =
+req.body;
+const nuevaEspecialidad = {
+    especialidad, 
+    descripcion,
+     fecha_creacion, 
+     estado,   
+};
+await onformdata.especialidad.create(nuevaEspecialidad);
+res.redirect("/");
+
 }
+
+especialidades.list = async (req, res) => {
+    const especialidades = await sql.query("SELECT * FROM especialidades");
+    console.log(especialidades);
+    res.render("modules/especialidades", { especialidades });
+  };
 
 module.exports = especialidades
