3ff709ad73f45f8046589c25736ed9ee8570ed26
Thalia Guzñay <jtc.guznay@yavirac.edu.ec>
2022-04-19 19:57:14 -0500

    crud_especialidades

--- salutest-business/src/rutas/especialidades.js
+++ salutest-business/src/rutas/especialidades.js
@@ -5,5 +5,7 @@ const {check_login} = require('../lib/auth');
 const especialidadesController = require("../controllers/especialidades.controller");
 
 rutas.get('/especialidades',check_login, especialidadesController.list);
+rutas.get("/especialidades",check_login, especialidadesController.mostrar);
+rutas.post("/add",especialidadesController.save);
 
 module.exports = rutas;
