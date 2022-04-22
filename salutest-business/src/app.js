de0123a550b2857382512c108558af3a45162664
Thalia Guzñay <jtc.guznay@yavirac.edu.ec>
2022-04-22 10:21:52 -0500

    ruta_componentes

--- salutest-business/src/app.js
+++ salutest-business/src/app.js
@@ -75,5 +75,5 @@ app.use(require("./rutas/solicitudes"));
 app.use(require("./rutas/solicitudes_componentes"));
 app.use(require("./rutas/instituciones_medicas"));
 app.use(require("./rutas/general_configuration"));
-
+app.use(require("./rutas/modulo_componentes"));
 module.exports = app;
