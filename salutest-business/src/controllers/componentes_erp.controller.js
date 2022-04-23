809b56f02275763a763bac22daf933256642716f
Jenny <86363759+jennifher23@users.noreply.github.com>
2022-04-23 16:44:54 -0500

    Funcidad

--- salutest-business/src/controllers/componentes_erp.controller.js
+++ salutest-business/src/controllers/componentes_erp.controller.js
@@ -1,7 +1,29 @@
-const componentes_erp = {}
+const orm = require("../config-database/database.orm");
+const sql = require("../config-database/database.sql")
 
-componentes_erp.mostrar = (req,res) => {
+const componente_erp = {}
+
+componente_erp.list = async (req,res) => {
+    
+    const componentes_erp = await sql.query(
+        "select * from componentes c join aplicacions a on c.aplicacionIdAplicacion = a.id_aplicacion WHERE a.nombre_aplicacion = 'Salutest ERP'"
+    );
+
     res.render ('modules/componentes_erp')
+
 }
 
-module.exports = componentes_erp
+componente_erp.list_default = async (req, res) => {
+
+    const componentes_erp = await sql.query(
+        "select * from componentes c join aplicacions a on c.aplicacionIdAplicacion = a.id_aplicacion WHERE a.nombre_aplicacion = 'Salutest ERP' and c.default_componente = 1"
+    );
+
+    res.render('modules/componentes_erp', {
+        componentes_erp,
+    });
+
+
+}
+
+module.exports = componente_erp
