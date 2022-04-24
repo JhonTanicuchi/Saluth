f922e05e2bf7343ea922c5fb9b1a1ebe827747e4
JhonTanicuchi <jav.tanicuchi@yavirac.edu.ec>
2022-04-24 07:10:01 -0500

    relaciones muchos a muchos y creaion de relacion instituciones con catalogos

--- salutest-business/src/controllers/solicitudes.controller.js
+++ salutest-business/src/controllers/solicitudes.controller.js
@@ -22,7 +22,7 @@ Handlebars.registerHelper("conditional_solicitudes_awaiting", function (list) {
   return list.length == 0;
 });
 
-solicitud.list = async (req, res) => {  
+solicitud.list = async (req, res) => {
   const solicitudes = await sql.query("SELECT * FROM solicituds");
 
   let date = new Date();
@@ -62,49 +62,59 @@ solicitud.approve = async (req, res) => {
   const username = user_generator.create.usuarioemail;
   const password = "1111";
 
-
   const id_solicitud = req.params.id;
 
-  sql.query(
+  const solicitud = await sql.query(
     "SELECT * FROM solicituds WHERE id_solicitud = ?",
-    [id_solicitud],
-    function (error, results) {
-      if (error) throw error;
-      const nuevaInstitucionMedica = {
-        nombre: results[0].nombre_institucion_medica,
-      };
-
-      email.create(
-        req,
-        res,
-        results[0].nombre_contacto,
-        results[0].email_contacto,
-        username,
-        password
-      );
-      orm.institucion_medica.create(nuevaInstitucionMedica);
-    }
+    [id_solicitud]
   );
-
-  sql.query(
+  await sql.query("INSERT INTO institucion_medicas (nombre) VALUES (?)", [
+    solicitud[0].nombre_institucion_medica,
+  ]);
+  const solicitud_institucion = await sql.query(
     "select * from solicituds s join institucion_medicas i on s.nombre_institucion_medica = i.nombre WHERE s.id_solicitud = ?",
-    [id_solicitud],
-    async function (error, results) {
-      if (error) throw error;
-      const updateSolicitud = {
-        estado: "aprobado",
-        institucionMedicaIdInstitucionMedica: results[0].id_institucion_medica,
-      };
-      console.log(results[0])
-      await orm.solicitud
-        .findOne({ where: { id_solicitud: id_solicitud } })
-        .then((solicitudes) => {
-          solicitudes.update(updateSolicitud);
-        });
-    }
+    [id_solicitud]
   );
 
-  
+   await sql.query(
+     "INSERT INTO institucion_catalogo (catalogoIdCatalogo,institucionMedicaIdInstitucionMedica) VALUES (?,?)",
+     [
+       solicitud_institucion[0].tipo_institucion ,
+       solicitud_institucion[0].id_institucion_medica,
+     ]
+   );
+   await sql.query(
+     "INSERT INTO institucion_catalogo (catalogoIdCatalogo,institucionMedicaIdInstitucionMedica) VALUES (?,?)",
+     [
+       solicitud_institucion[0].categoria_institucion,
+       solicitud_institucion[0].id_institucion_medica,
+     ]
+   );
+
+
+
+  console.log(solicitud_institucion[0]);
+
+  const updateSolicitud = {
+    estado: "aprobado",
+    institucionMedicaIdInstitucionMedica:
+      solicitud_institucion[0].id_institucion_medica,
+  };
+
+  await orm.solicitud
+    .findOne({ where: { id_solicitud: id_solicitud } })
+    .then((solicitudes) => {
+      solicitudes.update(updateSolicitud);
+    });
+
+  email.create(
+    req,
+    res,
+    solicitud[0].nombre_contacto,
+    solicitud[0].email_contacto,
+    username,
+    password
+  );
 
   res.redirect("/solicitudes");
 };
