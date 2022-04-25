d774a9759e28931b4b8b9bd9cd7632c32fbc7cfb
joeiv023 <jic.guevara@yavirac.edu.ec>
2022-04-24 23:34:34 -0500

    correcion de sintaxis roles

--- salutest-management/src/public/js/roles.js
+++ salutest-management/src/public/js/roles.js
@@ -12,8 +12,7 @@ function onSubmit() {
 
 function Leer() {
     let DataForm = {}
-    DataForm["rol"] = document.getElementById("rol").value
-    DataForm["ced"] = document.getElementById("ced").value
+    DataForm["id"] = document.getElementById("id").value
     DataForm["nom"] = document.getElementById("nom").value
     DataForm["ape"] = document.getElementById("ape").value
     DataForm["edad"] = document.getElementById("edad").value
@@ -23,22 +22,20 @@ function Leer() {
 function InsertarDatos(data) {
     let table = document.getElementById("tabla_roles").getElementsByTagName('tbody')[0]
     let Fila = table.insertRow(table.length)
-    columna1 = Fila.insertCell(0).innerHTML = data.rol
-    columna1 = Fila.insertCell(1).innerHTML = data.ced
-    columna1 = Fila.insertCell(2).innerHTML = data.nom
-    columna2 = Fila.insertCell(3).innerHTML = data.ape
-    columna3 = Fila.insertCell(4).innerHTML = data.edad
-    columna3 = Fila.insertCell(5).innerHTML = `<input class="editar" type="button" onClick="Editarr(this)" value="Editar" >
+    columna1 = Fila.insertCell(0).innerHTML = data.id
+    columna1 = Fila.insertCell(1).innerHTML = data.nom
+    columna2 = Fila.insertCell(2).innerHTML = data.ape
+    columna3 = Fila.insertCell(3).innerHTML = data.edad
+    columna3 = Fila.insertCell(4).innerHTML = `<input class="editar" type="button" onClick="Editarr(this)" value="Editar" >
                                             <input class="eliminar" type="button" onClick="Borrarr(this)" value="Eliminar" >`
-    document.getElementById("rol").focus()
+    document.getElementById("id").focus()
     Vaciar()
 }
 
 
 
 function Vaciar() {
-    document.getElementById("rol").value = ""
-    document.getElementById("ced").value = ""
+    document.getElementById("id").value = ""
     document.getElementById("nom").value = ""
     document.getElementById("ape").value = ""
     document.getElementById("edad").value = ""
@@ -47,20 +44,18 @@ function Vaciar() {
 
 function Editarr(td) {
     Fila = td.parentElement.parentElement
-    document.getElementById("rol").value = Fila.cells[0].innerHTML
-    document.getElementById("ced").value = Fila.cells[1].innerHTML
-    document.getElementById("nom").value = Fila.cells[2].innerHTML
-    document.getElementById("ape").value = Fila.cells[3].innerHTML
-    document.getElementById("edad").value = Fila.cells[4].innerHTML
+    document.getElementById("id").value = Fila.cells[0].innerHTML
+    document.getElementById("nom").value = Fila.cells[1].innerHTML
+    document.getElementById("ape").value = Fila.cells[2].innerHTML
+    document.getElementById("edad").value = Fila.cells[3].innerHTML
 }
 
 function Actualizar(DataForm) {
     Fila.cells[0].innerHTML = DataForm.rol
-    Fila.cells[1].innerHTML = DataForm.ced
-    Fila.cells[2].innerHTML = DataForm.nom
-    Fila.cells[3].innerHTML = DataForm.ape
-    Fila.cells[4].innerHTML = DataForm.edad
-    document.getElementById("ced").focus()
+    Fila.cells[1].innerHTML = DataForm.nom
+    Fila.cells[2].innerHTML = DataForm.ape
+    Fila.cells[3].innerHTML = DataForm.edad
+    document.getElementById("nom").focus()
 }
 
 function Borrarr(td) {
